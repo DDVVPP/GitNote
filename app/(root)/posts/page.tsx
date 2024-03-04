88c2b17d604c9334429c0ac4244dde6d882b5cf0
DDVVPP <darshinvp@gmail.com>
2024-03-03 19:35:37 -0800

    update: updated Posts component

--- app/(root)/posts/page.tsx
+++ app/(root)/posts/page.tsx
@@ -4,16 +4,15 @@ import Link from 'next/link';
 const Posts = () => {
   return (
     <>
-      <h1>Recent Posts</h1>
-      <Link href="/posts/1">
+      {/* <h1>Recent Posts</h1> */}
+      {/* Map through all posts */}
+      <Link className='paragraph-3-medium text-white-300' href="/posts/1">
         Post1
       </Link>
-      <br/>
-      <Link href="/posts/2">
+      <Link className='paragraph-3-medium text-white-300' href="/posts/2">
         Post2
       </Link>
-      <br/>
-      <Link href="/posts/3">
+      <Link className='paragraph-3-medium text-white-300' href="/posts/3">
         Post3
       </Link>
     </>
