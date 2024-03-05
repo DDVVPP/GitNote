4d9684f07a2873a3018163bb3054b654686bab5c
DDVVPP <darshinvp@gmail.com>
2024-03-05 13:24:47 -0800

    cleanup: cleaned up home page

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,11 +1,13 @@
-import Posts from "../posts/page";
-
+import Posts from '../posts/page';
+import TempBadgeTest from '@/components/shared/TempBadgeTest';
 
 export default function Home() {
   return (
-    <div className='paragraph-3-medium text-white-300 flex flex-col gap-2'>
-      <h1 className='display-2-bold text-white-100'>Recent Posts</h1>
-      <Posts/>
+    <div className="paragraph-3-medium text-white-300 flex flex-col gap-2">
+      <h1 className="display-2-bold text-white-100">Recent Posts</h1>
+      <TempBadgeTest />
+
+      {/* <Posts /> */}
     </div>
   );
 }
