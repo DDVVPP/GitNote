3b26c9dcfab25043d6f1872cc29f822d143948e2
DDVVPP <darshinvp@gmail.com>
2024-03-05 13:54:43 -0800

    update: added outline variant

--- components/shared/TempBadgeTest.tsx
+++ components/shared/TempBadgeTest.tsx
@@ -2,17 +2,36 @@ import Badge from '../shared/ui/Badge';
 
 const TempBadgeTest = () => (
   <div className="flex flex-col gap-4">
-    <Badge icon="dot" color="blue" size="small">
+    <Badge variant="outline" icon="dot" color="blue" size="small">
       Badge Blue Dot Small
     </Badge>
-    <Badge icon="info" color="red" size="small">
+    <Badge variant="outline" icon="info" color="orange" size="medium">
+      Badge Orange Info Medium
+    </Badge>
+    <Badge variant="outline" icon="info" color="red" size="small">
       Badge Red Info Small
     </Badge>
-    <Badge icon="dot" color="green" size="medium">
+    <Badge variant="outline" icon="dot" color="green" size="medium">
       Badge Green Dot Medium
     </Badge>
-    <Badge icon="info" color="orange" size="medium">
+    <Badge variant="outline" icon="dot" color="gray" size="medium">
+      Badge Green Dot Medium
+    </Badge>
+
+    <Badge variant="solid" icon="dot" color="blue" size="small">
+      Badge Blue Dot Small
+    </Badge>
+    <Badge variant="solid" icon="info" color="orange" size="medium">
       Badge Orange Info Medium
+    </Badge>
+    <Badge variant="solid" icon="info" color="red" size="small">
+      Badge Red Info Small
+    </Badge>
+    <Badge variant="solid" icon="dot" color="green" size="medium">
+      Badge Green Dot Medium
+    </Badge>
+    <Badge variant="solid" icon="dot" color="gray" size="medium">
+      Badge Green Dot Medium
     </Badge>
 
     <Badge>Badge All Defaults</Badge>
--- components/shared/ui/Badge.tsx
+++ components/shared/ui/Badge.tsx
@@ -2,8 +2,9 @@ import { DotIcon, InfoIcon } from 'lucide-react';
 
 interface BadgeProps extends React.PropsWithChildren {
   icon?: 'info' | 'dot';
-  color?: 'blue' | 'orange' | 'red' | 'green';
+  color?: 'blue' | 'orange' | 'red' | 'green' | 'gray';
   size?: 'small' | 'medium';
+  variant?: 'outline' | 'solid';
 }
 
 const colorClassMap = {
@@ -11,7 +12,17 @@ const colorClassMap = {
   orange: 'bg-orange-100 text-orange-900',
   red: 'bg-red-100 text-red-900',
   green: 'bg-green-100 text-green-800',
+  gray: 'bg-gray-100 text-gray-900',
   default: 'bg-gray-100 text-gray-900',
+};
+
+const outlineColorClassMap = {
+  blue: 'border border-blue-200 text-blue-800',
+  orange: 'border border-orange-200 text-orange-900',
+  red: 'border border-red-200 text-red-900',
+  green: 'border border-green-500 text-green-800',
+  gray: 'border border-gray-100/20 text-gray-600',
+  default: 'border border-gray-100/20 text-gray-600',
 };
 
 const textSizeClassMap = {
@@ -35,15 +46,16 @@ const IconMap = {
 const baseclass =
   'flex items-center content-center px-2 py-1 rounded-2xl w-min text-nowrap';
 
-const Badge = ({ children, icon, color, size }: BadgeProps) => {
+const Badge = ({ children, icon, color, size, variant }: BadgeProps) => {
   const colorClass = colorClassMap[color || 'default'];
+  const outlineColorClass = outlineColorClassMap[color || 'default'];
   const textSizeClass = textSizeClassMap[size || 'default'];
   const iconSizeClass = iconSizeClassMap[size || 'default'];
   const IconComponent = IconMap[icon || 'default'];
 
   return (
     <div
-      className={`${baseclass} ${colorClass} ${textSizeClass} ${icon !== 'dot' ? 'gap-1' : 'pr-4'}`}
+      className={`${baseclass} ${textSizeClass} ${variant === 'outline' ? outlineColorClass : colorClass} ${icon !== 'dot' ? 'gap-1' : 'pr-4'} `}
     >
       {IconComponent && (
         <IconComponent size={icon === 'dot' ? '24px' : iconSizeClass} />
