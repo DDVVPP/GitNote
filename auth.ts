7eac6515cb56f3904cd527300e9c25adda251b84
DDVVPP <darshinvp@gmail.com>
2024-03-07 09:53:11 -0800

    setup: added prividers to list of providers and missing jwt strategy

--- auth.ts
+++ auth.ts
@@ -1,5 +1,9 @@
 import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
+import LinkedInProvider from 'next-auth/providers/linkedin';
+import TwitterProvider from 'next-auth/providers/twitter';
+
 import { PrismaAdapter } from '@auth/prisma-adapter';
 import { prisma } from '@/db';
 import { Role } from '@prisma/client';
@@ -16,6 +20,9 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
+  session: {
+    strategy: 'jwt',
+  },
   adapter: PrismaAdapter(prisma),
   pages: {
     newUser: '/sign-up/onboarding',
@@ -25,5 +32,33 @@ export const {
       clientId: process.env.GITHUB_CLIENT_ID!,
       clientSecret: process.env.GITHUB_CLIENT_SECRET!,
     }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID!,
+      clientSecret: process.env.GOOGLE_SECRET!,
+    }),
+    LinkedInProvider({
+      clientId: process.env.LINKEDIN_CLIENT_ID!,
+      clientSecret: process.env.LINKEDIN_CLIENT_SECRET!,
+    }),
+    TwitterProvider({
+      clientId: process.env.TWITTER_CLIENT_ID!,
+      clientSecret: process.env.TWITTER_CLIENT_SECRET!,
+    }),
   ],
+  callbacks: {
+    async jwt({ token, user }) {
+      //everytime read or write to the token
+      if (user) {
+        token.role = user.role;
+      }
+      return token; //this token will get passed to session
+    },
+    async session({ session, token }) {
+      //will run this also in jwt
+      if (token) {
+        session.user.role = token.role;
+      }
+      return session;
+    },
+  },
 });
