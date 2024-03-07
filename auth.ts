15ff01e773192ef3ae900884cb3b61df0aacc939
DDVVPP <darshinvp@gmail.com>
2024-03-07 00:12:38 -0800

    setup: authentication

---
+++ auth.ts
@@ -0,0 +1,29 @@
+import NextAuth from 'next-auth';
+import GithubProvider from 'next-auth/providers/github';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import { prisma } from '@/db';
+import { Role } from '@prisma/client';
+
+declare module 'next-auth' {
+  interface User {
+    role: Role;
+  }
+}
+
+export const {
+  handlers: { GET, POST },
+  auth,
+  signIn,
+  signOut,
+} = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  pages: {
+    newUser: '/sign-up/onboarding',
+  },
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    }),
+  ],
+});
