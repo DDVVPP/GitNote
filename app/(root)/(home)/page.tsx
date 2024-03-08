d73a287784aec2e65d3cc5f6424fef9e33e124d8
DDVVPP <darshinvp@gmail.com>
2024-03-07 23:28:28 -0800

    test: added user actions to client side to test them

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,25 +1,27 @@
 import { auth } from '@/auth';
-import { signIn, signOut } from '@/lib/actions';
+import { getUser } from '@/lib/actions/user.actions';
 
 import Posts from '../posts/page';
 
 export default async function Home() {
   const session = await auth();
+  const userEmail = session && (await session.user?.email);
+  const user = userEmail && (await getUser(userEmail));
 
   return (
-    <div className="paragraph-3-medium text-white-300 flex flex-col gap-2">
-      <form action={signIn}>
-        <button type="submit">Sign In</button>
-      </form>
-
-      <form action={signOut}>
-        <button type="submit">Sign Out</button>
-      </form>
-
-      {session?.user ? <div>Signed In</div> : <div>No user...</div>}
-
-      <h1 className="display-2-bold text-white-100">Recent Posts</h1>
-      {session ? <Posts session={session} /> : <div>Eeeee no session</div>}
-    </div>
+    <>
+      <div className="text-white-300 flex flex-col gap-4">
+        <h1 className="display-2-bold text-white-100">
+          Hi {user && user.name}!
+        </h1>
+        <p className="paragraph-3-medium">
+          You have the role of {user && user.role}
+        </p>
+      </div>
+      <div className="text-white-300 flex flex-col mt-4 gap-4">
+        <h3 className="display-2-bold text-white-100">Recent Posts</h3>
+        {session && <Posts user={user} />}
+      </div>
+    </>
   );
 }
