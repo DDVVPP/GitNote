6486fc5f192be99f2c7ce130f85e92a1c558e89f
DDVVPP <darshinvp@gmail.com>
2024-03-11 14:05:20 -0700

    update: added Gitnote to main auth layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
 
-const Layout = ({children}: {children: React.ReactNode}) => {
+import Image from 'next/image';
+import gitNoteIcon from '@/public/gitNoteIcon.svg';
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className="flex min-h-screen w-full items-center justify-center">{children}</main>
-  )
-}
+    <main className="flex flex-col items-center justify-center max-w-full">
+      <div className="flex space-x-2 mt-14 items-center">
+        <Image src={gitNoteIcon} alt="Git Note Icon" />
+        <h1 className="text-left text-[22.55px] font-bold leading-[19.64px]">
+          GitNote
+        </h1>
+      </div>
+      <div className="flex min-h-screen">{children}</div>
+    </main>
+  );
+};
 
-export default Layout
+export default Layout;
