5c38ea11a458eb7d4fb39b6d5bb576cf217514ff
DDVVPP <darshinvp@gmail.com>
2024-03-11 20:51:30 -0700

    feat: initalized Availability, BasicInfo, Knowledge, Learning onboarding step components

--- app/(auth)/sign-up/onboarding/page.tsx
+++ app/(auth)/sign-up/onboarding/page.tsx
@@ -1,9 +1,42 @@
-import React from 'react'
+'use client';
+
+import BasicInformation from '@/components/onboarding/BasicInformation';
+import LearningGoals from '@/components/onboarding/LearningGoals';
+import KnowledgeLevel from '@/components/onboarding/KnowledgeLevel';
+import Availability from '@/components/onboarding/Availability';
+
+import React, { useEffect } from 'react';
+import { useState } from 'react';
 
 const Onboarding = () => {
-  return (
-    <div>Onboarding</div>
-  )
-}
+  const [step, setStep] = useState(1);
 
-export default Onboarding
+  const renderStep = () => {
+    switch (step) {
+      case 1:
+        return <BasicInformation setStep={setStep} />;
+      case 2:
+        return (
+          <div className="space-y-4">
+            <LearningGoals setStep={setStep} />;
+          </div>
+        );
+      case 3:
+        return (
+          <div className="space-y-4">
+            <KnowledgeLevel setStep={setStep} />;
+          </div>
+        );
+      case 4:
+        return (
+          <div className="space-y-4">
+            <Availability setStep={setStep} />;
+          </div>
+        );
+    }
+  };
+
+  return <div className="flex flex-col justify-center">{renderStep()}</div>;
+};
+
+export default Onboarding;
