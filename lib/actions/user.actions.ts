b2a2e97d62b6967f59b150c747cf07607b012a0e
DDVVPP <darshinvp@gmail.com>
2024-03-19 18:45:40 -0700

    update: messy attempt at updated Goals model

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -2,7 +2,7 @@
 
 import { auth } from '@/auth';
 import { prisma } from '@/db';
-import { User } from '@prisma/client';
+import { User, Goals } from '@prisma/client';
 import bcryptjs from 'bcryptjs';
 
 export async function createUser(data: Partial<User>) {
@@ -45,8 +45,35 @@ export async function updateUser(data: Partial<User>) {
   const email = session && session.user?.email;
   if (!email) return;
   console.log('DATA', data);
-  try {
-    if (data) {
+
+  if (data && data.goals) {
+    try {
+      const user = prisma.user.findUnique({
+        where: {
+          email,
+        },
+      });
+      if (data && user) {
+        const goals = prisma.goals.upsert({
+          where: {
+            user,
+            userId: user.id,
+          },
+          create: {
+            data,
+          },
+          update: {
+            data,
+          },
+        });
+        return { goals, error: null };
+      }
+    } catch (error) {
+      console.error('Error updating user:', error);
+      return { error: 'An unexpected error occurred while updating user.' };
+    }
+  } else if (data && !data.goals) {
+    try {
       const user = prisma.user.update({
         where: {
           email,
@@ -54,10 +81,37 @@ export async function updateUser(data: Partial<User>) {
         data,
       });
       return { user, error: null };
+
+      // if (data) {
+      //   const user = prisma.user.upsert({
+      //     where: {
+      //       email,
+      //     },
+      //     create: {
+      //       goals: data.goals,
+      //     },
+      //     update: {
+      //       goals: data.goals,
+      //     },
+      //   });
+      // const user = prisma.user.update({
+      //   where: {
+      //     email,
+      //   },
+      //   data: {
+      //     upsert: {
+      //       set: {
+      //         goals: data.goals,
+      //       },
+      //     },
+      //   },
+      // });
+      // return { user, error: null };
+      // }
+    } catch (error) {
+      console.error('Error updating user:', error);
+      return { error: 'An unexpected error occurred while updating user.' };
     }
-  } catch (error) {
-    console.error('Error updating user:', error);
-    return { error: 'An unexpected error occurred while updating user.' };
   }
   return { error: 'An unexpected error occurred while updating user.' };
 }
