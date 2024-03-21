3112440150b4d58c1c0dc2a20af6b0030adfea63
DDVVPP <darshinvp@gmail.com>
2024-03-21 01:47:17 -0700

    feat: availability component with react-datepicker

--- components/onboarding/Availability.tsx
+++ components/onboarding/Availability.tsx
@@ -1,10 +1,23 @@
 'use client';
 
-import { Dispatch, SetStateAction } from 'react';
+import ReactDatePicker from 'react-datepicker';
+import { Controller } from 'react-hook-form';
+import 'react-datepicker/dist/react-datepicker.css';
 
-import Datepicker from '../shared/ui/Datepicker';
+const Availability = ({
+  register,
+  control,
+  formState,
+  watch,
+}: {
+  register: any;
+  control: any;
+  formState: any;
+  watch: any;
+}) => {
+  const availability = watch('availability');
 
-const Availability = () => {
+  console.log('availability watch', availability);
   return (
     <>
       <h1 className="display-2-bold mb-4">Schedule & Availability</h1>
@@ -12,16 +25,65 @@ const Availability = () => {
         <input
           type="checkbox"
           className="appearance-none border border-white-500 h-3 w-3 bg-black-700 rounded-sm cursor-pointer text-green-400"
+          {...register('availability')}
         />
         <label className="paragraph-3-regular  text-white-300">
           Are you available for a new project?
         </label>
       </section>
+      {availability && (
+        <section className="flex mt-2 justify-between display-none mb-4 space-x-4">
+          <div className="flex flex-col space-y-2">
+            <span className="paragraph-3-regular  text-white-300">
+              Start Date & Time
+            </span>
+            <Controller
+              control={control}
+              name="startDate"
+              render={({ field }) => (
+                <ReactDatePicker
+                  className="bg-black-700 text-white-300 border-none paragraph-3-regular px-4 py-4"
+                  onChange={(e) => field.onChange(e)}
+                  selected={field.value}
+                  showTimeSelect
+                  timeFormat="HH:mm"
+                  timeIntervals={15}
+                  timeCaption="time"
+                  dateFormat="MM/dd/yyyy h:mm aa"
+                />
+              )}
+            />
+            <span className="paragraph-4-regular text-white-500">
+              The time is in your local timezone
+            </span>
+          </div>
 
-      <section className="flex mt-2 justify-between display-none">
-        <Datepicker label="Start Date & Time" />
-        <Datepicker label="End Date & Time" />
-      </section>
+          <div className="flex flex-col space-y-2">
+            <span className="paragraph-3-regular  text-white-300">
+              End Date & Time
+            </span>
+            <Controller
+              control={control}
+              name="endDate"
+              render={({ field }) => (
+                <ReactDatePicker
+                  className="bg-black-700 text-white-300 border-none paragraph-3-regular px-4 py-4 cursor-pointer"
+                  onChange={(e) => field.onChange(e)}
+                  selected={field.value}
+                  showTimeSelect
+                  timeFormat="HH:mm"
+                  timeIntervals={15}
+                  timeCaption="time"
+                  dateFormat="MM/dd/yyyy h:mm aa"
+                />
+              )}
+            />
+            <span className="paragraph-4-regular text-white-500">
+              The time is in your local timezone
+            </span>
+          </div>
+        </section>
+      )}
     </>
   );
 };
