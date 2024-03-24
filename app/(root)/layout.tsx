8be43b0cfec37a37d9980b8d44a2f1df07186587
DDVVPP <darshinvp@gmail.com>
2024-03-23 19:10:34 -0700

    fix: style updates to overall layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,13 +1,13 @@
-import SideNavbar from "@/components/side-navbar/SideNavbar";
+import LeftNavbar from "@/components/left-navbar/LeftNavbar";
 import RightSidebar from "@/components/right-sidebar/RightSidebar";
 import React from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main>
-      <div className="flex h-screen justify-start">
-        <SideNavbar />
-        <section className="flex min-h-screen flex-1 flex-col pb-10 pt-20 max-md:pb-14 sm:px-14">
+      <div className="flex min-h-screen justify-start">
+        <LeftNavbar />
+        <section className="flex min-h-screen flex-1 flex-col pt-20 max-md:pb-14 sm:px-14">
           <div className="mx-auto w-full max-w-5xl">{children}</div>
         </section>
         <RightSidebar />
--- app/(root)/posts/page.tsx
+++ app/(root)/posts/page.tsx
@@ -1,80 +1,35 @@
-'use client';
+"use client";
 
-import React from 'react';
-// import Link from 'next/link';
-import { useState } from 'react';
-import { updateUser, deleteUser } from '@/lib/actions/user.actions';
+import React from "react";
+import Link from "next/link";
+// import { deleteUser } from "@/lib/actions/user.actions";
 
-const Posts = (user: any) => {
-  console.log('USER', user);
-  const [data, setData] = useState({
-    id: user && user.id,
-    name: user && user.name,
-    email: user && user.email,
-  });
+const Posts = () => {
+  // const handleDelete = () => {
+  //   const userId = user.id;
 
-  const handleSubmit = () => {
-    const userId = user.user.id;
-    setData({
-      ...data,
-      id: userId,
-    });
+  //   if (userId) {
+  //     deleteUser(userId);
+  //   }
+  // };
 
-    if (data && data.id) {
-      updateUser(data);
-    }
-  };
-
-  const handleDelete = () => {
-    const userId = user.user.id;
-
-    if (userId) {
-      deleteUser(userId);
-    }
-  };
   return (
     <>
-      {/* <h1>Recent Posts</h1> */}
+      <h1>Recent Posts</h1>
       {/* Map through all posts */}
-      {/* <Link href="/posts/1">Post 1</Link>
+      <Link href="/posts/1">Post 1</Link>
       <Link href="/posts/2">Post2</Link>
-      <Link href="/posts/3">Post3</Link> */}
+      <Link href="/posts/3">Post3</Link>
 
-      <div className="mt-10 text-black-600">
-        <form action={handleSubmit}>
-          <label>Name</label>
-          <input
-            onChange={(event) => {
-              console.log(event.target.value);
-              setData({
-                ...data,
-                name: event.target.value,
-              });
-            }}
-            type="text"
-            name="name"
-          />
-          <label>Email</label>
-          <input
-            onChange={(event) => {
-              console.log(event.target.value);
-              setData({
-                ...data,
-                email: event.target.value,
-              });
-            }}
-            type="text"
-            name="email"
-          />
-          <button type="submit">Submit</button>
-        </form>
-
-        <div className="mt-8">
-          <button onClick={() => handleDelete()} type="submit">
-            Delete User
-          </button>
+      {/* {user?.role === "ADMIN" && (
+        <div className="text-black-600 mt-10">
+          <div className="mt-8">
+            <button onClick={() => handleDelete()} type="submit">
+              Delete User
+            </button>
+          </div>
         </div>
-      </div>
+      )} */}
     </>
   );
 };
