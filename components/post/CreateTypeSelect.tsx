15477b933fc0c2080d4135ced3f532de1d6cc38e
DDVVPP <darshinvp@gmail.com>
2024-04-02 14:43:55 -0700

    fix: moved ref again for more accuracy

--- components/post/CreateTypeSelect.tsx
+++ components/post/CreateTypeSelect.tsx
@@ -57,36 +57,38 @@ const CreateTypeSelect = ({
   };
 
   return (
-    <div ref={ref}>
+    <div>
       <label className="paragraph-3-medium text-white-300 mb-2 flex flex-col">
         Create Type
       </label>
-      <button
-        type="button"
-        className="bg-black-700 text-white-300 paragraph-3-medium flex w-full items-center justify-between rounded p-3"
-        onClick={() => setIsOpen((curr) => !curr)}
-      >
-        {renderSelection()}
-        {isOpen ? <ChevronUp size={17} /> : <ChevronDown size={17} />}
-      </button>
-      {isOpen && (
-        <div className="paragraph-3-medium bg-black-700 mt-0.5 flex flex-col gap-y-2 rounded p-2">
-          {createTypeList.map((createType) => {
-            const { icon: Icon, uiName, color } = createType;
-            return (
-              <ul
-                className={`flex cursor-pointer gap-x-2 ${color} hover:bg-black-600 p-1 hover:rounded hover:p-1`}
-                onClick={() =>
-                  onSelectClick(createType as CreateTypeListItemType)
-                }
-              >
-                <Icon size={20} />
-                {uiName}
-              </ul>
-            );
-          })}
-        </div>
-      )}
+      <div ref={ref}>
+        <button
+          type="button"
+          className="bg-black-700 text-white-300 paragraph-3-medium flex w-full items-center justify-between rounded p-3"
+          onClick={() => setIsOpen((curr) => !curr)}
+        >
+          {renderSelection()}
+          {isOpen ? <ChevronUp size={17} /> : <ChevronDown size={17} />}
+        </button>
+        {isOpen && (
+          <div className="paragraph-3-medium bg-black-700 mt-0.5 flex flex-col gap-y-2 rounded p-2">
+            {createTypeList.map((createType) => {
+              const { icon: Icon, uiName, color } = createType;
+              return (
+                <ul
+                  className={`flex cursor-pointer gap-x-2 ${color} hover:bg-black-600 p-1 hover:rounded hover:p-1`}
+                  onClick={() =>
+                    onSelectClick(createType as CreateTypeListItemType)
+                  }
+                >
+                  <Icon size={20} />
+                  {uiName}
+                </ul>
+              );
+            })}
+          </div>
+        )}
+      </div>
     </div>
   );
 };
