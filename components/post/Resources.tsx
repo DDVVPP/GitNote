848eb4afa233d38d6dea365bbef548dee7a2d3b6
DDVVPP <darshinvp@gmail.com>
2024-04-02 22:24:13 -0700

    fix: style cleanup

--- components/post/Resources.tsx
+++ components/post/Resources.tsx
@@ -22,54 +22,55 @@ const Resources = ({
   return (
     <section>
       <div className="text-white-300 flex flex-col">
-        <h3 className="paragraph-3-medium text-white-500 mb-6">
-          RESOURCES & LINKS
-        </h3>
+        <h3 className="paragraph-3-medium text-white-500">RESOURCES & LINKS</h3>
       </div>
 
-      {fields.map((field: { id: number }, index: number) => {
-        return (
-          <React.Fragment key={field.id}>
-            <div className="mb-2 flex gap-x-2">
-              <div className="flex w-full flex-col">
-                <input
-                  type="text"
-                  className="placeholder:text-white-500 paragraph-3-regular bg-black-700 w-full rounded-md border-none p-3 focus:outline-none"
-                  placeholder="Label"
-                  {...register(`resources.${index}.label`)}
-                />
+      <section className="mt-6">
+        {fields.map((field: { id: number }, index: number) => {
+          return (
+            <React.Fragment key={field.id}>
+              <div className="mb-2 flex gap-x-2">
+                <div className="flex w-full flex-col">
+                  <input
+                    type="text"
+                    className="placeholder:text-white-500 paragraph-3-regular bg-black-700 w-full rounded-md border-none p-3 focus:outline-none"
+                    placeholder="Label"
+                    {...register(`resources.${index}.label`)}
+                  />
 
-                {errors.resources &&
-                  errors.resources[index]?.label?.message && (
-                    <span className="text-error-500 paragraph-3-regular">
-                      {errors.resources[index].label.message}
-                    </span>
-                  )}
+                  {errors.resources &&
+                    errors.resources[index]?.label?.message && (
+                      <span className="text-error-500 paragraph-3-regular">
+                        {errors.resources[index].label.message}
+                      </span>
+                    )}
+                </div>
+                <div className="flex w-full flex-col">
+                  <input
+                    type="text"
+                    className="placeholder:text-white-500 paragraph-3-regular bg-black-700 w-full rounded-md border-none p-3 focus:outline-none"
+                    placeholder="Resource Link"
+                    {...register(`resources.${index}.link`)}
+                  />
+                  {errors.resources &&
+                    errors.resources[index]?.link?.message && (
+                      <span className="text-error-500 paragraph-3-regular">
+                        {errors.resources[index].link.message}
+                      </span>
+                    )}
+                </div>
+                <button className="bg-black-700 h-11 rounded px-3">
+                  <X
+                    className="text-white-500"
+                    size={16}
+                    onClick={() => remove(index)}
+                  />
+                </button>
               </div>
-              <div className="flex w-full flex-col">
-                <input
-                  type="text"
-                  className="placeholder:text-white-500 paragraph-3-regular bg-black-700 w-full rounded-md border-none p-3 focus:outline-none"
-                  placeholder="Resource Link"
-                  {...register(`resources.${index}.link`)}
-                />
-                {errors.resources && errors.resources[index]?.link?.message && (
-                  <span className="text-error-500 paragraph-3-regular">
-                    {errors.resources[index].link.message}
-                  </span>
-                )}
-              </div>
-              <button className="bg-black-700 h-11 rounded px-3">
-                <X
-                  className="text-white-500"
-                  size={16}
-                  onClick={() => remove(index)}
-                />
-              </button>
-            </div>
-          </React.Fragment>
-        );
-      })}
+            </React.Fragment>
+          );
+        })}
+      </section>
 
       <div className="mt-4">
         <Button
