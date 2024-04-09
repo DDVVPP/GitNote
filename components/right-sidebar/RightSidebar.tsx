73a2949b109ab8e5edfb9d614cf3fd2f3922d9df
DDVVPP <darshinvp@gmail.com>
2024-04-08 18:27:12 -0700

    feat: renderCorrect right sidebar info

--- components/right-sidebar/RightSidebar.tsx
+++ components/right-sidebar/RightSidebar.tsx
@@ -2,17 +2,36 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname, useParams } from "next/navigation";
+
 import { Image as LandscapeIcon } from "lucide-react";
 
-// import SidebarTags from "./SidebarTags";
-// import SidebarRelatedPosts from "./SidebarRelatedPosts";
-// import SidebarOnThisPage from "./SidebarOnThisPage";
+import SidebarTags from "./SidebarTags";
+import SidebarRelatedPosts from "./SidebarRelatedPosts";
 import SidebarSocialMedia from "./SidebarSocialMedia";
 import { User } from "@prisma/client";
 
 const RightSidebar = ({ user }: { user: User }) => {
+  const pathname = usePathname();
+  const params = useParams();
+
+  const renderCorrectSidebar = () => {
+    switch (pathname) {
+      case "/":
+        return <SidebarTags />;
+      case "/posts/create-post":
+        return <SidebarTags />;
+      case `/posts/${params.postId}`:
+        return <SidebarRelatedPosts />;
+      case "/profile":
+        return <SidebarSocialMedia user={user} />;
+      case "/profile/edit":
+        return <SidebarSocialMedia user={user} />;
+    }
+  };
+
   return (
-    <div className=" bg-black-800 flex-2 sticky right-0 top-0 z-20 flex h-screen w-1/5 flex-col gap-10 overflow-y-auto p-8 pt-20">
+    <div className=" bg-black-800 flex-2 sticky right-0 top-0 z-20 flex h-screen w-1/5 flex-col gap-10 overflow-y-auto p-8 pt-16">
       <Link href="/profile" className="flex items-center gap-2">
         {user.image ? (
           <Image
@@ -37,10 +56,7 @@ const RightSidebar = ({ user }: { user: User }) => {
         </div>
       </Link>
 
-      {/* <SidebarTags /> */}
-      {/* <SidebarRelatedPosts /> */}
-      {/* <SidebarOnThisPage /> */}
-      <SidebarSocialMedia user={user} />
+      {renderCorrectSidebar()}
     </div>
   );
 };
