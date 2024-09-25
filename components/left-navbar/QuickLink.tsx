08b448e5975c33faf604d01a02dbbc57e6e266cf
DDVVPP <darshinvp@gmail.com>
2024-09-25 16:31:48 -0700

    fix: QuickLink hover state

--- components/left-navbar/QuickLink.tsx
+++ components/left-navbar/QuickLink.tsx
@@ -3,8 +3,6 @@ import { QuickLinkProps } from "@/types";
 
 const QuickLink = ({ groupName, href, name, icon: Icon }: QuickLinkProps) => {
   const groupNameClass = groupName === "jsm" ? "group/jsm" : "group/github";
-  const groupNameHoverClass =
-    groupName === "jsm" ? "group-hover/jsm" : "group-hover/github";
 
   return (
     <a
@@ -13,14 +11,18 @@ const QuickLink = ({ groupName, href, name, icon: Icon }: QuickLinkProps) => {
       className={`${groupNameClass} flex h-8 items-center gap-x-2`}
     >
       <Icon
-        className={
+        className={`${
           groupName === "jsm"
-            ? `group-hover/jsm:fill-white-100 fill-[#55597D] group-hover:duration-300`
-            : `group-hover/github:stroke-white-100 stroke-[#55597D] group-hover:duration-300`
-        }
+            ? `group-hover/jsm:fill-white-100 fill-[#55597D] `
+            : `group-hover/github:stroke-white-100 stroke-[#55597D]`
+        } group-hover:duration-300`}
       />
       <p
-        className={`${groupNameHoverClass}:text-white-100 group-hover:duration-300`}
+        className={`${
+          groupName === "jsm"
+            ? "group-hover/jsm:text-white-100"
+            : "group-hover/github:text-white-100"
+        } group-hover:duration-300`}
       >
         {name}
       </p>
