77125e2b93b248109a7237dcff82ba6e128479d5
DDVVPP <darshinvp@gmail.com>
2024-09-25 16:27:03 -0700

    update: create/update post hover states

--- components/shared/ui/Button.tsx
+++ components/shared/ui/Button.tsx
@@ -14,11 +14,12 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 }
 
 const colorClassMap = {
-  blue: "bg-primary-500 text-black-900 paragraph-3-bold ",
-  gray: "bg-black-600 text-white-100 paragraph-3-medium",
+  blue: "bg-primary-500 text-black-900 paragraph-3-bold hover:text-white-100 hover:duration-300 hover:bg-blue-500",
+  gray: "bg-black-600 hover:bg-[#4d567a] text-white-100 paragraph-3-medium hover:duration-300",
   darkGrayWhiteText: "bg-black-700 text-white-300 paragraph-3-medium",
   darkGrayBlueText: "bg-black-700 text-primary-500 paragraph-3-medium",
-  gradient: "text-white-100 primary-gradient paragraph-4-medium",
+  gradient:
+    "text-white-100 primary-gradient paragraph-4-medium hover:duration-300",
   red: "bg-error-500 text-white-100 paragraph-3-medium",
   default: "bg-black-700 text-white-300 paragraph-3-medium",
 };
