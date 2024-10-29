ce27e75812b4282c15c5aabb71b9dbc6916b4db5
DDVVPP <darshinvp@gmail.com>
2024-10-28 21:46:34 -0700

    update: added comments in createPosts

--- prisma/seed/posts.ts
+++ prisma/seed/posts.ts
@@ -2,7 +2,7 @@ import { CreateType } from "@prisma/client";
 import { createPost } from "./postHelpers";
 const { faker } = require("@faker-js/faker");
 
-export async function createPosts(emails, demoUserEmail) {
+export async function createPosts(emails: String[]) {
   // USER POSTS
   for (let i = 0; i < 20; i++) {
     const userEmail = faker.helpers.arrayElement(emails);
@@ -12,10 +12,14 @@ export async function createPosts(emails, demoUserEmail) {
 
   // DEMO USER POSTS
   const createTypes = Object.values(CreateType);
+  const demoUserEmail = "demouser@email.com";
+
+  // Create one post for each type
   createTypes.forEach(async (type) => {
     await createPost(demoUserEmail, type);
   });
 
+  // Create more posts with random number of types
   const additionalPostsCount = 40 - createTypes.length;
   for (let i = 0; i < additionalPostsCount; i++) {
     const createType = faker.helpers.arrayElement(createTypes);
