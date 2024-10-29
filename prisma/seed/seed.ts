e458d49aebc65bcf0f20c34f87992fe6fd273bba
DDVVPP <darshinvp@gmail.com>
2024-10-28 21:47:56 -0700

    update: added createUsers and createPosts query to seed file

--- prisma/seed/seed.ts
+++ prisma/seed/seed.ts
@@ -1,8 +1,13 @@
+import { createPosts } from "./posts";
+import { createUsers } from "./users";
+
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 async function main() {
   try {
+    const emails = await createUsers();
+    await createPosts(emails);
   } catch (error) {
     console.error("Error seeding data:", error);
   } finally {
