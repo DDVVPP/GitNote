1c9611e1e1281f0573c07169e54532a54bfdc425
DDVVPP <darshinvp@gmail.com>
2024-10-28 21:46:16 -0700

    update: added comments in postHelpers

--- prisma/seed/postHelpers.ts
+++ prisma/seed/postHelpers.ts
@@ -5,18 +5,21 @@ const prisma = new PrismaClient();
 const { faker } = require("@faker-js/faker");
 
 export async function createPost(
-  userEmail: string,
+  email: string,
   createType: string | CreateType
 ) {
+  // between 2 to 5 elements
   const randomNumberOfArrayElements = Math.floor(Math.random() * 4) + 2;
   const learningsOrSteps = Array.from(
     { length: randomNumberOfArrayElements },
     () => faker.lorem.sentence()
   );
 
+  // between 1 to 5 paragraphs
   const randomParagraphsCount = Math.floor(Math.random() * 5) + 1;
   const content = faker.lorem.paragraphs(randomParagraphsCount);
 
+  // between 0 to 5 resources
   const randomResourcesCount = Math.floor(Math.random() * 5);
   const resources = Array.from({ length: randomResourcesCount }, () => ({
     label: faker.lorem.words(3),
@@ -42,7 +45,7 @@ export async function createPost(
       ...(createType === "KNOWLEDGE" && { learnings: learningsOrSteps }),
       user: {
         connect: {
-          email: userEmail,
+          email,
         },
       },
       tags: faker.helpers.arrayElements(techTags, { min: 1, max: 10 }),
