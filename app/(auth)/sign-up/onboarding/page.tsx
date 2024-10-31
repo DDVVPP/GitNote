c38b4268982eadf0fd6f504df863eb370e534de9
DDVVPP <darshinvp@gmail.com>
2024-10-31 13:29:03 -0700

    init: initial attempt at mobile responsiveness

--- app/(auth)/sign-up/onboarding/page.tsx
+++ app/(auth)/sign-up/onboarding/page.tsx
@@ -1,4 +1,4 @@
-import React, {  Suspense } from "react";
+import React, { Suspense } from "react";
 import { User } from "@prisma/client";
 import Onboarding from "@/components/onboarding/Onboarding";
 import { getUser } from "@/lib/actions/user.actions";
@@ -6,7 +6,13 @@ import { getUser } from "@/lib/actions/user.actions";
 const OnboardingWrapper = async () => {
   const user = (await getUser()) as User;
 
-  return <Suspense><Onboarding user={user} /></Suspense>;
+  return (
+    <div className="bg-black-800 flex w-full flex-col justify-center border md:w-[600px]">
+      <Suspense>
+        <Onboarding user={user} />
+      </Suspense>
+    </div>
+  );
 };
 
 export default OnboardingWrapper;
