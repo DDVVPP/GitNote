8fc47a820687de67a587245f24fe8a1dca87e270
DDVVPP <darshinvp@gmail.com>
2024-10-30 22:14:56 -0700

    fix: removed unneccessary margin prop

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -3,7 +3,7 @@ import ProviderLogins from "@/components/ProviderLogins";
 
 const Login = () => {
   return (
-    <div className="mt-0 flex w-[400px] flex-col justify-center max-md:mx-6">
+    <div className="flex w-[400px] flex-col justify-center max-md:mx-6">
       <BasicAuthLogin />
       <section>
         <ProviderLogins />
