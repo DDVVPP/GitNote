2a8aefaf867ace4dfc5d2bf51ad764d427d5c4a9
DDVVPP <darshinvp@gmail.com>
2024-11-05 23:07:50 -0800

    fix:eslint error

--- components/MobileHeader.tsx
+++ components/MobileHeader.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import gitNoteIcon from "@/public/gitNoteIcon.svg";
