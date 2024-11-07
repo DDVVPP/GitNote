271b76573df7012dbc8f80dfc44911e07deb9da1
DDVVPP <darshinvp@gmail.com>
2024-11-06 18:40:20 -0800

    style: updated left navbar vertical spacing

--- components/left-navbar/LeftNavbar.tsx
+++ components/left-navbar/LeftNavbar.tsx
@@ -14,58 +14,60 @@ import { QuickLinkProps } from "@/types";
 
 const LeftNavbar = () => {
   return (
-    <nav className="flex h-screen flex-col justify-between px-7 pt-10">
-      <section>
-        <div className="border-white-500 flex flex-col justify-start gap-y-12 border-b">
-          <Link href="/">
-            <div className="flex items-center gap-x-2">
-              <Image src={gitNoteIcon} alt="Git Note Icon" />
-              <h1 className="text-left text-[22.55px] font-bold leading-[19.64px] hover:text-blue-500 hover:duration-300">
-                GitNote
-              </h1>
-            </div>
-          </Link>
-
-          <div className="space-y-4 pb-12">
-            <Link href="/posts/create-post">
-              <Button icon="plus" color="gradient">
-                Create Post
-              </Button>
-            </Link>
-            <Suspense>
-              <Search />
-            </Suspense>
+    <nav className="flex h-screen flex-col px-7 pt-10">
+      <section className="border-white-500 flex flex-col justify-start gap-y-12">
+        <Link href="/">
+          <div className="flex items-center gap-x-2">
+            <Image src={gitNoteIcon} alt="Git Note Icon" />
+            <h1 className="text-left text-[22.55px] font-bold leading-[19.64px] hover:text-blue-500 hover:duration-300">
+              GitNote
+            </h1>
           </div>
+        </Link>
+
+        <div className="space-y-4">
+          <Link href="/posts/create-post">
+            <Button icon="plus" color="gradient">
+              Create Post
+            </Button>
+          </Link>
+          <Suspense>
+            <Search />
+          </Suspense>
         </div>
-
-        <NavSection title="POSTS"> placeholder for posts</NavSection>
-
-        <NavSection title="QUICK LINKS">
-          {quickLinks.map((link: QuickLinkProps) => {
-            return (
-              <QuickLink
-                key={link.groupName}
-                groupName={link.groupName}
-                href={link.href}
-                name={link.name}
-                icon={link.icon}
-              />
-            );
-          })}
-        </NavSection>
       </section>
 
-      <section className="mb-5 flex align-bottom">
-        <form action={signOut}>
-          <button
-            className="paragraph-3-medium text-white-100 hover:text-white-100 flex gap-2 hover:duration-300"
-            type="submit"
-          >
-            <Image src={logoutIcon} alt="Logout Icon" />
-            Logout
-          </button>
-        </form>
-      </section>
+      <hr className="dark:bg-black-700 my-6 h-px w-full border-0" />
+
+      <NavSection title="POSTS"> placeholder for posts</NavSection>
+
+      <hr className="dark:bg-black-700 my-6 h-px w-full border-0" />
+
+      <NavSection title="QUICK LINKS">
+        {quickLinks.map((link: QuickLinkProps) => {
+          return (
+            <QuickLink
+              key={link.groupName}
+              groupName={link.groupName}
+              href={link.href}
+              name={link.name}
+              icon={link.icon}
+            />
+          );
+        })}
+      </NavSection>
+
+      <hr className="dark:bg-black-700 my-6 h-px w-full border-0" />
+
+      <form action={signOut}>
+        <button
+          className="paragraph-3-medium text-white-100 hover:text-white-100 flex gap-2 hover:duration-300"
+          type="submit"
+        >
+          <Image src={logoutIcon} alt="Logout Icon" />
+          Logout
+        </button>
+      </form>
     </nav>
   );
 };
