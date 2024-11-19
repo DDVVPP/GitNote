abbb42e9ee627d4a625a181f0f8024910a0298ce
DDVVPP <darshinvp@gmail.com>
2024-11-18 19:30:17 -0800

    wip: brute force resources&links mobile

--- components/post/Resources.tsx
+++ components/post/Resources.tsx
@@ -21,19 +21,19 @@ const Resources = ({
 
   return (
     <section>
-      <div className="flex flex-col text-white-300">
+      <div className="text-white-300 flex flex-col">
         <h3 className="paragraph-3-medium text-white-500">RESOURCES & LINKS</h3>
       </div>
 
       <section className="mt-6">
         {fields.map((field: { id: number }, index: number) => {
           return (
-            <React.Fragment key={field.id}>
-              <div className="mb-2 flex gap-x-2">
+            <div key={field.id} className="mb-2 flex gap-x-2">
+              <div className="max-xs-b:hidden flex w-full gap-x-2">
                 <div className="flex w-full flex-col">
                   <input
                     type="text"
-                    className="paragraph-3-regular w-full rounded-md border-none bg-black-700 p-3 placeholder:text-white-500 focus:outline-none"
+                    className="paragraph-3-regular bg-black-700 placeholder:text-white-500 w-full rounded-md border-none p-3 focus:outline-none"
                     placeholder="Label"
                     {...register(`resources.${index}.label`)}
                   />
@@ -48,7 +48,7 @@ const Resources = ({
                 <div className="flex w-full flex-col">
                   <input
                     type="text"
-                    className="paragraph-3-regular w-full rounded-md border-none bg-black-700 p-3 placeholder:text-white-500 focus:outline-none"
+                    className="paragraph-3-regular bg-black-700 placeholder:text-white-500 w-full rounded-md border-none p-3 focus:outline-none"
                     placeholder="Resource Link"
                     {...register(`resources.${index}.link`)}
                   />
@@ -61,7 +61,7 @@ const Resources = ({
                 </div>
                 <button
                   type="button"
-                  className="h-11 rounded bg-black-700 px-3"
+                  className="bg-black-700 h-11 rounded px-3"
                   onClick={() => remove(index)}
                 >
                   <X
@@ -70,7 +70,51 @@ const Resources = ({
                   />
                 </button>
               </div>
-            </React.Fragment>
+
+              <div className="xs-b:hidden flex w-full flex-col gap-y-2">
+                <div className="flex gap-x-2">
+                  <div className="flex w-full flex-col">
+                    <input
+                      type="text"
+                      className="paragraph-3-regular bg-black-700 placeholder:text-white-500 w-full rounded-md border-none p-3 focus:outline-none"
+                      placeholder="Label"
+                      {...register(`resources.${index}.label`)}
+                    />
+
+                    {errors.resources &&
+                      errors.resources[index]?.label?.message && (
+                        <span className="error-message">
+                          {errors.resources[index].label.message}
+                        </span>
+                      )}
+                  </div>
+                  <button
+                    type="button"
+                    className="bg-black-700 h-11 rounded px-3"
+                    onClick={() => remove(index)}
+                  >
+                    <X
+                      className="text-white-500 hover:text-white-300 hover:duration-300"
+                      size={16}
+                    />
+                  </button>
+                </div>
+                <div className="flex w-full flex-col">
+                  <input
+                    type="text"
+                    className="paragraph-3-regular bg-black-700 placeholder:text-white-500 w-full rounded-md border-none p-3 focus:outline-none"
+                    placeholder="Resource Link"
+                    {...register(`resources.${index}.link`)}
+                  />
+                  {errors.resources &&
+                    errors.resources[index]?.link?.message && (
+                      <span className="error-message">
+                        {errors.resources[index].link.message}
+                      </span>
+                    )}
+                </div>
+              </div>
+            </div>
           );
         })}
       </section>
