592ef74609fe42bd65df01d95c0c02fb7e0c0c89
DDVVPP <darshinvp@gmail.com>
2024-11-18 20:02:39 -0800

    wip: extracted mobile fields into separate file

---
+++ components/post/ResourceFieldsMobile.tsx
@@ -0,0 +1,63 @@
+import { X } from "lucide-react";
+import React from "react";
+
+type ResourceFieldType = {
+  errors: { resources: { label: { message: string } }[] };
+  index: number;
+  register: (type: string) => {};
+  remove: (index: number) => void;
+};
+
+const ResourceFieldsMobile = ({
+  errors,
+  index,
+  register,
+  remove,
+}: ResourceFieldType) => {
+  return (
+    <>
+      <div className="flex gap-x-2">
+        <div className="flex w-full flex-col">
+          <input
+            type="text"
+            className="paragraph-3-regular bg-black-700 placeholder:text-white-500 w-full rounded-md border-none p-3 focus:outline-none"
+            placeholder="Label"
+            {...register(`resources.${index}.label`)}
+          />
+
+          {errors.resources && errors.resources[index]?.label?.message && (
+            <span className="error-message">
+              {errors.resources[index].label.message}
+            </span>
+          )}
+        </div>
+        <button
+          type="button"
+          className="bg-black-700 h-11 rounded px-3"
+          onClick={() => remove(index)}
+        >
+          <X
+            className="text-white-500 hover:text-white-300 hover:duration-300"
+            size={16}
+          />
+        </button>
+      </div>
+
+      <div className="flex w-full flex-col">
+        <input
+          type="text"
+          className="paragraph-3-regular bg-black-700 placeholder:text-white-500 w-full rounded-md border-none p-3 focus:outline-none"
+          placeholder="Resource Link"
+          {...register(`resources.${index}.link`)}
+        />
+        {errors.resources && errors.resources[index]?.link?.message && (
+          <span className="error-message">
+            {errors.resources[index].link.message}
+          </span>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default ResourceFieldsMobile;
--- components/post/Resources.tsx
+++ components/post/Resources.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { X } from "lucide-react";
 
 import { Button } from "../shared";
+import ResourceFieldsMobile from "./ResourceFieldsMobile";
 
 const Resources = ({
   useFieldArray,
@@ -72,47 +73,12 @@ const Resources = ({
               </div>
 
               <div className="xs-b:hidden flex w-full flex-col gap-y-2">
-                <div className="flex gap-x-2">
-                  <div className="flex w-full flex-col">
-                    <input
-                      type="text"
-                      className="paragraph-3-regular bg-black-700 placeholder:text-white-500 w-full rounded-md border-none p-3 focus:outline-none"
-                      placeholder="Label"
-                      {...register(`resources.${index}.label`)}
-                    />
-
-                    {errors.resources &&
-                      errors.resources[index]?.label?.message && (
-                        <span className="error-message">
-                          {errors.resources[index].label.message}
-                        </span>
-                      )}
-                  </div>
-                  <button
-                    type="button"
-                    className="bg-black-700 h-11 rounded px-3"
-                    onClick={() => remove(index)}
-                  >
-                    <X
-                      className="text-white-500 hover:text-white-300 hover:duration-300"
-                      size={16}
-                    />
-                  </button>
-                </div>
-                <div className="flex w-full flex-col">
-                  <input
-                    type="text"
-                    className="paragraph-3-regular bg-black-700 placeholder:text-white-500 w-full rounded-md border-none p-3 focus:outline-none"
-                    placeholder="Resource Link"
-                    {...register(`resources.${index}.link`)}
-                  />
-                  {errors.resources &&
-                    errors.resources[index]?.link?.message && (
-                      <span className="error-message">
-                        {errors.resources[index].link.message}
-                      </span>
-                    )}
-                </div>
+                <ResourceFieldsMobile
+                  errors={errors}
+                  index={index}
+                  register={register}
+                  remove={remove}
+                />
               </div>
             </div>
           );
