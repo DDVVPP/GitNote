5aca7e1e8285981aec0eae7ba3167d6b946e82d4
DDVVPP <darshinvp@gmail.com>
2024-11-21 21:14:05 -0800

    update: added inputBlurHandler to Steps, Title, and Input component

--- components/post/Steps.tsx
+++ components/post/Steps.tsx
@@ -2,8 +2,9 @@
 
 import React, { useEffect, useState } from "react";
 
-import { Button } from "@/components/shared/ui";
 import { CheckSquare, X } from "lucide-react";
+import { Button } from "@/components/shared/ui";
+import useInputBlurHandler from "@/lib/utils/useInputBlurHandler";
 
 const Steps = ({
   useFormHelpers,
@@ -30,6 +31,8 @@ const Steps = ({
       : setIsDisabled(false);
   }, [fieldsInput, formState]);
 
+  useInputBlurHandler("steps");
+
   return (
     <section className="space-y-6">
       <label className="paragraph-3-medium text-white-300">
@@ -38,12 +41,13 @@ const Steps = ({
       {fields.map((field: { id: number }, index: number) => {
         return (
           <React.Fragment key={field.id}>
-            <div className="my-2 flex items-center justify-between bg-black-700 px-3 py-1">
+            <div className="bg-black-700 my-2 flex items-center justify-between px-3 py-1">
               <div className="flex items-center space-x-2">
                 <CheckSquare className="text-primary-500" size={16} />
               </div>
               <input
-                className="paragraph-3-regular placeholder:paragraph-3-regular ml-2 w-full rounded-md border-none bg-black-700 pl-1 text-white-100 placeholder:text-white-300 focus:outline-none"
+                id="steps"
+                className="paragraph-3-regular placeholder:paragraph-3-regular bg-black-700 text-white-100 placeholder:text-white-300 ml-2 w-full rounded-md border-none pl-1 focus:outline-none"
                 placeholder="Enter a step"
                 {...register(`steps.${index}`)}
               />
