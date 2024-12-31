62cbaff9986e5fcd69b2c15ab6cef9448e097a3d
DDVVPP <darshinvp@gmail.com>
2024-12-30 23:50:17 -0800

    debug: added headers as arg to all getUser calls

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,4 +1,5 @@
 import { Suspense } from "react";
+import { headers } from "next/headers";
 import { getUser } from "@/lib/actions/user.actions";
 import { getAllPosts } from "@/lib/actions/post.actions";
 
@@ -13,7 +14,11 @@ export default async function Home({
 }: {
   searchParams: SearchParams;
 }) {
-  const user = (await getUser()) as User;
+  const reqHeaders = headers();
+  const headersObject = Object.fromEntries(reqHeaders.entries());
+  console.log("Headers in ProfileWrapper as Object:", headersObject);
+
+  const user = (await getUser(reqHeaders)) as User;
   if (!user) return <UserNotFound />;
 
   const posts = await getAllPosts({
--- app/(root)/profile/edit/page.tsx
+++ app/(root)/profile/edit/page.tsx
@@ -2,9 +2,13 @@ import { EditProfile } from "@/components/profile";
 import UserNotFound from "@/components/shared/UserNotFound";
 import { getUser } from "@/lib/actions/user.actions";
 import { User } from "@prisma/client";
-
+import { headers } from "next/headers";
 const EditProfileWrapper = async () => {
-  const user = (await getUser()) as User;
+  const reqHeaders = headers();
+  const headersObject = Object.fromEntries(reqHeaders.entries());
+  console.log("Headers in ProfileWrapper as Object:", headersObject);
+
+  const user = (await getUser(reqHeaders)) as User;
   if (!user) return <UserNotFound />;
 
   return <EditProfile user={user} />;
