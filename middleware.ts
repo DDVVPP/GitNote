32387604bc663a439cca84d6706769b507bbbf4a
DDVVPP <darshinvp@gmail.com>
2025-01-02 12:19:53 -0800

    debug: added more console statements

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,20 @@
 import { auth } from "@/auth";
 import { NextResponse } from "next/server";
 
-export default auth((req) => {
-  // const { role, onboardingStatus } = req.auth?.user || {};
-  // const { pathname } = req.nextUrl;
+export default auth(async (req) => {
+  const { role, onboardingStatus } = (await req.auth?.user) || {};
+  const { pathname } = req.nextUrl;
   // console.log("Pathname:>>>>>>>>", pathname);
   // console.log("Auth User:>>>>>>>>", req.auth?.user);
   // console.log("Cookies:>>>>>>>>", req.cookies);
   // console.log("Decoded JWT in middleware:", req.auth);
-  // if (!req.auth?.user) {
-  //   console.log("User not authenticated. Redirecting to login.");
-  //   return NextResponse.redirect(`${req.nextUrl.origin}/login`);
-  // }
+  if (req.auth?.user) {
+    console.log("Auth User:>>>>>>>>", req.auth?.user);
+  }
+  if (!req.auth?.user) {
+    console.log("User not authenticated. Redirecting to login.");
+    return NextResponse.redirect(`${req.nextUrl.origin}/login`);
+  }
   // if (pathname === "/" && !role) {
   //   console.log("in middleware>>>>>>>", req.auth?.user);
   //   return NextResponse.redirect(`${req.nextUrl.origin}/login`);
