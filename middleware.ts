7ff2e50d1da4cc2379c9b130842b362b76319c94
DDVVPP <darshinvp@gmail.com>
2025-01-02 12:11:00 -0800

    debug: testing if middleware is the issue

--- middleware.ts
+++ middleware.ts
@@ -2,47 +2,47 @@ import { auth } from "@/auth";
 import { NextResponse } from "next/server";
 
 export default auth((req) => {
-  const { role, onboardingStatus } = req.auth?.user || {};
-  const { pathname } = req.nextUrl;
-  console.log("Pathname:>>>>>>>>", pathname);
-  console.log("Auth User:>>>>>>>>", req.auth?.user);
-  console.log("Cookies:>>>>>>>>", req.cookies);
-  console.log("Decoded JWT in middleware:", req.auth);
-  if (!req.auth?.user) {
-    console.log("User not authenticated. Redirecting to login.");
-    return NextResponse.redirect(`${req.nextUrl.origin}/login`);
-  }
-  if (pathname === "/" && !role) {
-    console.log("in middleware>>>>>>>", req.auth?.user);
-    return NextResponse.redirect(`${req.nextUrl.origin}/login`);
-  }
+  // const { role, onboardingStatus } = req.auth?.user || {};
+  // const { pathname } = req.nextUrl;
+  // console.log("Pathname:>>>>>>>>", pathname);
+  // console.log("Auth User:>>>>>>>>", req.auth?.user);
+  // console.log("Cookies:>>>>>>>>", req.cookies);
+  // console.log("Decoded JWT in middleware:", req.auth);
+  // if (!req.auth?.user) {
+  //   console.log("User not authenticated. Redirecting to login.");
+  //   return NextResponse.redirect(`${req.nextUrl.origin}/login`);
+  // }
+  // if (pathname === "/" && !role) {
+  //   console.log("in middleware>>>>>>>", req.auth?.user);
+  //   return NextResponse.redirect(`${req.nextUrl.origin}/login`);
+  // }
 
-  if (pathname === "/profile" && !req.auth?.user) {
-    console.error("User not authenticated. Redirecting to login.");
-    console.error("Auth Object:", req.auth);
-    console.error("Cookies:", req.cookies);
-    return NextResponse.redirect(`${req.nextUrl.origin}/login`);
-  }
-  if (pathname === "/profile" && !role) {
-    console.log(req.auth?.user);
-    return NextResponse.redirect(`${req.nextUrl.origin}/login`);
-  }
-  if (pathname === "/profile/edit" && !req.auth?.user) {
-    return NextResponse.redirect(`${req.nextUrl.origin}/login`);
-  }
+  // if (pathname === "/profile" && !req.auth?.user) {
+  //   console.error("User not authenticated. Redirecting to login.");
+  //   console.error("Auth Object:", req.auth);
+  //   console.error("Cookies:", req.cookies);
+  //   return NextResponse.redirect(`${req.nextUrl.origin}/login`);
+  // }
+  // if (pathname === "/profile" && !role) {
+  //   console.log(req.auth?.user);
+  //   return NextResponse.redirect(`${req.nextUrl.origin}/login`);
+  // }
+  // if (pathname === "/profile/edit" && !req.auth?.user) {
+  //   return NextResponse.redirect(`${req.nextUrl.origin}/login`);
+  // }
 
-  if (pathname === "/admin" && role !== "ADMIN") {
-    return NextResponse.redirect(`${req.nextUrl.origin}/`);
-  }
+  // if (pathname === "/admin" && role !== "ADMIN") {
+  //   return NextResponse.redirect(`${req.nextUrl.origin}/`);
+  // }
 
-  if (!onboardingStatus) {
-    return NextResponse.redirect(`${req.nextUrl.origin}/sign-up/onboarding`);
-  }
-  if (onboardingStatus < 4) {
-    return NextResponse.redirect(
-      `${req.nextUrl.origin}/sign-up/onboarding?step=${onboardingStatus}`
-    );
-  }
+  // if (!onboardingStatus) {
+  //   return NextResponse.redirect(`${req.nextUrl.origin}/sign-up/onboarding`);
+  // }
+  // if (onboardingStatus < 4) {
+  //   return NextResponse.redirect(
+  //     `${req.nextUrl.origin}/sign-up/onboarding?step=${onboardingStatus}`
+  //   );
+  // }
   return NextResponse.next();
 });
 
