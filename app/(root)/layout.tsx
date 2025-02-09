faab16018245b381ae67b88c3c862044c783aeee
DDVVPP <darshinvp@gmail.com>
2025-02-09 13:52:43 -0800

    cleanup: removed PostContext

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -6,7 +6,6 @@ import { getUser } from "@/lib/actions/user.actions";
 import { getAllPosts } from "@/lib/actions/post.actions";
 import MobileHeader from "@/components/MobileHeader";
 import UserNotFound from "@/components/shared/UserNotFound";
-import { PostContextProvider } from "@/lib/context/PostContext";
 
 const Layout = async ({ children }: { children: React.ReactNode }) => {
   const user = (await getUser()) as User;
@@ -17,23 +16,21 @@ const Layout = async ({ children }: { children: React.ReactNode }) => {
 
   return user && somePosts ? (
     <div className="flex h-screen flex-col">
-      <PostContextProvider>
-        <header className="max-[1220px]:block lg:hidden">
-          <MobileHeader user={user} posts={somePosts as Post[]} />
-        </header>
+      <header className="max-[1220px]:block lg:hidden">
+        <MobileHeader user={user} posts={somePosts as Post[]} />
+      </header>
 
-        <div className="flex flex-1 flex-col lg:flex-row">
-          <nav className="bg-black-800 hidden w-[290px] lg:block">
-            <LeftNavbar posts={somePosts as Post[]} />
-          </nav>
+      <div className="flex flex-1 flex-col lg:flex-row">
+        <nav className="bg-black-800 hidden w-[290px] lg:block">
+          <LeftNavbar posts={somePosts as Post[]} />
+        </nav>
 
-          <main className="flex-1 px-7 pt-10">{children}</main>
+        <main className="flex-1 px-7 pt-10">{children}</main>
 
-          <nav className="bg-black-800 hidden w-[290px] lg:block">
-            <RightSidebar user={user} />
-          </nav>
-        </div>
-      </PostContextProvider>
+        <nav className="bg-black-800 hidden w-[290px] lg:block">
+          <RightSidebar user={user} />
+        </nav>
+      </div>
     </div>
   ) : (
     <UserNotFound />
